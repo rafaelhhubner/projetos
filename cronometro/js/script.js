0020d9d6d5dab9beecd9064b06f83bce6030e307
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-19 17:26:07 -0300

    Concluding

--- cronometro/js/script.js
+++ cronometro/js/script.js
@@ -3,7 +3,7 @@ const secondsEl = document.querySelector("#seconds")
 const millisecondsEl = document.querySelector("#milliseconds")
 const startBtn = document.querySelector("#startBtn")
 const pauseBtn = document.querySelector("#pauseBtn")
-const resumeBtn = document.querySelector("resumeBtn")
+const resumeBtn = document.querySelector("#resumeBtn")
 const resetBtn = document.querySelector('#resetBtn')
 
 let interval;
@@ -12,13 +12,15 @@ let seconds = 0;
 let milliseconds = 0;
 let isPaused = false;
 
-startBtn.addEventListener("click", startTimer)
+startBtn.addEventListener("click", startTimer);
 
 function startTimer() {
-
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer)
     interval = setInterval(() => {
 
-        if(isPaused) {
+        if(!isPaused) {
             milliseconds += 10;
 
             if(milliseconds === 1000) {
@@ -30,9 +32,50 @@ function startTimer() {
                 seconds = 0;
             }
 
-            minutesEl.textContent = minutes;
-            secondsEl.textContent = seconds;
-            millisecondsEl.textContent = milliseconds;
+            minutesEl.textContent = formatTime(minutes);
+            secondsEl.textContent = formatTime(seconds);
+            millisecondsEl.textContent = formatMilliseconds(milliseconds);
         }
     }, 10);
+
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+}
+
+function pauseTimer() {
+    isPaused = true;
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "block";
+}
+
+function resumeTimer() {
+    isPaused = false;
+    pauseBtn.style.display = "block";
+    resumeBtn.style.display = "none";
+}
+
+function resetTimer() {
+    isPaused = false;
+    
+    clearInterval(interval);
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+
+    minutesEl.textContent = "00";
+    secondsEl.textContent = "00";
+    millisecondsEl.textContent = "000";
+
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "none";
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+function formatMilliseconds(time) {
+    return time < 100 ? `${time}`.padStart(3, "0") : time;
+
 }
