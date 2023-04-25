745e42545d2383392d69beed4ac0e8b3dfdbda76
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-25 00:46:04 -0300

    Project finished

--- landingpagedark/js/script.js
+++ landingpagedark/js/script.js
@@ -1,5 +1,28 @@
-const changeThemeBtn = document.querySelector("#change-theme")
+const changeThemeBtn = document.querySelector("#change-theme");
+
+// Toggle Dark Mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+// Load Light or Dark Mode
+function loadTheme() {
+    const DarkMode = localStorage.getItem("dark")
+
+    if(DarkMode) {
+        toggleDarkMode();
+    }
+}
+
+loadTheme()
 
 changeThemeBtn.addEventListener("change", function() {
-    document.body.classList.toggle("dark")
+    toggleDarkMode()
+
+    // Save or Remove Dark Mode
+    localStorage.removeItem("dark");
+
+    if(document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1);
+    } 
 })
