e6e33ff5e512ec9d72ec6e74232044c6f21f3ef3
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-04-29 17:44:22 -0300

    Navbar / Footer

--- react/costs/src/App.js
+++ react/costs/src/App.js
@@ -3,27 +3,28 @@ import Home from "./components/pages/Home"
 import Company from "./components/pages/Company"
 import Contact from "./components/pages/Contact"
 import NewProject from "./components/pages/NewProject"
+import Projects from "./components/pages/Projects"
 
 import Container from "./components/layout/Container"
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
 
 function App() {
   return (
     <Router>
       
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Companhia</Link>
-        <Link to="/newproject">Novo Projeto</Link>
+        <Navbar />
      
       <Container customClass="min-height">
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
             <Route path="/company" element={<Company />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
