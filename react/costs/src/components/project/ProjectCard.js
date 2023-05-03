df95592f5586bda243faea1938c6b3cd46d610a4
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-05-02 23:17:14 -0300

    Linking the projects from the database

---
+++ react/costs/src/components/project/ProjectCard.js
@@ -0,0 +1,23 @@
+import styles from "./ProjectCard.module.css"
+
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
+
+function ProjectCard({ id, name, budget, category, handleRemove}) {
+    return (
+        <div>
+            <h4>{name}</h4>
+            <p>
+                <span>Orçamento:</span> R${budget}
+            </p>
+            <p>
+                <span></span> {category}
+            </p>
+            <div>
+                <p>Editaer</p>
+                <p>Remover</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard
