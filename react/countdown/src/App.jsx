4614ed0115a424ef5af71496a6bba65e04a57e10
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-05-17 00:02:37 -0300

    Concluding 2nd part

--- react/countdown/src/App.jsx
+++ react/countdown/src/App.jsx
@@ -1,6 +1,8 @@
 import Title from './components/Title';
 import Counter from "./components/Counter";
 
+import NewYear from "./assets/newyear.jpg"
+
 import { useState } from 'react'
 import './App.css'
 
@@ -9,8 +11,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div className="App">
-        <div className="Container">
+      <div className="App" style={{backgroundImage: `url(${NewYear}) ` }}>
+        <div className="cfontainer">
           <Title title="Contagem regressiva para 2023"/>
           <div className="countdown-container">
             <Counter title="Dias" number={2}/>
