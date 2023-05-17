755a0bf2cbe36ab7a4870437b55d77422afba165
rafaelhhubner <126529307+rafaelhhubner@users.noreply.github.com>
2023-05-17 00:28:18 -0300

    Finishing Hook

--- react/countdown/src/hooks/useCountdown.jsx
+++ react/countdown/src/hooks/useCountdown.jsx
@@ -1,22 +1,38 @@
 import { useState } from 'react'
 
 const useCountdown = (date) => {
+    const [day, setDay] = useState()
+    const [hour, setHour] = useState()
+    const [minute, setMinute] = useState()
+    const [second, setSecond] = useState()
 
-    const countDate = new Date(date).getTime()
-    const now = new Date().getTime();
+    const countDown = () => {
+        const countDate = new Date(date).getTime()
+        const now = new Date().getTime();
 
-    const interval = countDate - now;
+        const interval = countDate - now;
 
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
 
-    const dayNumber = Math.floor(interval / day);
+        const dayNumber = Math.floor(interval / day);
+        const hourNumber = Math.floor((interval % day) / hour);
+        const minuteNumber = Math.floor((interval % hour) / minute);
+        const secondNumber = Math.floor((interval % minute) / second);
 
-    console.log(dayNumber);
+        setDay(dayNumber);
+        setHour(hourNumber);
+        setMinute(minuteNumber);
+        setSecond(secondNumber);
+    };
 
-    return 1;
+    setInterval(countDown, 1000);
+
+    console.log(dayNumber, hourNumber, minuteNumber, secondNumber);
+
+    return [day, hour, minute, second]
 }
 
 export default useCountdown
